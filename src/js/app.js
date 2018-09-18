a547df49389b5168b7ae301d4b88fecc3b7d069b
luckycjj <512170894@qq.com>
2018-09-18 15:12:31 +0800

    显示，计算精确度

--- src/js/app.js
+++ src/js/app.js
@@ -407,6 +407,55 @@ var androidIos = {
      }
 
   },
+  numAdd:function(num1, num2) {
+    //加法精度
+    var baseNum, baseNum1, baseNum2;
+    var precision;// 精度
+    try {
+      baseNum1 = num1.toString().split(".")[1].length;
+    } catch (e) {
+      baseNum1 = 0;
+    }
+    try {
+      baseNum2 = num2.toString().split(".")[1].length;
+    } catch (e) {
+      baseNum2 = 0;
+    }
+    baseNum = Math.pow(10, Math.max(baseNum1, baseNum2));
+    precision = (baseNum1 >= baseNum2) ? baseNum1 : baseNum2;
+    return ((num1 * baseNum - num2 * baseNum) / baseNum).toFixed(precision);
+  },
+  numSub:function (num1, num2) {
+    //减法精度
+    var baseNum, baseNum1, baseNum2;
+    var precision;// 精度
+    try {
+      baseNum1 = num1.toString().split(".")[1].length;
+    } catch (e) {
+      baseNum1 = 0;
+    }
+    try {
+      baseNum2 = num2.toString().split(".")[1].length;
+    } catch (e) {
+      baseNum2 = 0;
+    }
+    baseNum = Math.pow(10, Math.max(baseNum1, baseNum2));
+    precision = (baseNum1 >= baseNum2) ? baseNum1 : baseNum2;
+    return ((num1 * baseNum - num2 * baseNum) / baseNum).toFixed(precision);
+  },
+  numMulti:function(num1, num2) {
+    //乘法精度
+    var baseNum = 0;
+    try {
+      baseNum += num1.toString().split(".")[1].length;
+    } catch (e) {
+    }
+    try {
+      baseNum += num2.toString().split(".")[1].length;
+    } catch (e) {
+    }
+    return Number(num1.toString().replace(".", "")) * Number(num2.toString().replace(".", "")) / Math.pow(10, baseNum);
+  },
 };
 export {
   androidIos
